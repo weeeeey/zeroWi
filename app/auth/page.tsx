2dea195c12947edfbd1f1a35dbc2ef955d00ba0a
위영진 <qser155@naver.com>
2025-07-18 13:55:25 +0900

    refactor: separate oauth login button

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,33 +1,11 @@
 import { OauthLoginButton } from '@/components/pages/auth/oauth-login-button';
-import Image from 'next/image';
 
 function AuthPage() {
   return (
     <div className="space-y-4">
-      <KakaoButton />
-      <GoogleButton />
+      <OauthLoginButton host="kakao" />
+      <OauthLoginButton host="google" />
     </div>
-  );
-}
-
-function KakaoButton() {
-  return (
-    <OauthLoginButton className="bg-yellow-400">
-      <div className="relative size-8">
-        <Image src={'/images/kakao-logo.svg'} className="absolute" fill alt="카카오 로그인 버튼" />
-      </div>
-      <span className="font-bold">카카오 로그인</span>
-    </OauthLoginButton>
-  );
-}
-function GoogleButton() {
-  return (
-    <OauthLoginButton className="bg-slate-100 outline-2">
-      <div className="relative size-8">
-        <Image src={'/images/google-logo.svg'} className="absolute" fill alt="카카오 로그인 버튼" />
-      </div>
-      <span className="font-bold">구글 로그인</span>
-    </OauthLoginButton>
   );
 }
 
--- components/pages/auth/oauth-login-button.tsx
+++ components/pages/auth/oauth-login-button.tsx
@@ -1,15 +1,41 @@
 'use client';
 
 import { cn } from '@/lib/utils';
+import Image from 'next/image';
 import { useState } from 'react';
 
-interface OauthLoginButtonProps {
-  children: React.ReactNode;
-  className?: string;
+type OauthHost = 'kakao' | 'google';
+type OauthHostProps = {
+  labelText: string;
+  style: string;
+  imageUrl: string;
   handleLogin: () => void;
+};
+const OauthHosts: Record<OauthHost, OauthHostProps> = {
+  kakao: {
+    labelText: '카카오 계정으로 로그인',
+    style: 'bg-yellow-400',
+    imageUrl: '/images/kakao-logo.svg',
+    handleLogin: () => {
+      console.log('kakao');
+    },
+  },
+  google: {
+    labelText: '구글 계정으로 로그인',
+    style: 'bg-slate-100 outline-2',
+    imageUrl: '/images/google-logo.svg',
+    handleLogin: () => {
+      console.log('google');
+    },
+  },
+};
+
+interface OauthLoginButtonProps {
+  host: OauthHost;
 }
-export function OauthLoginButton({ children, className, handleLogin }: OauthLoginButtonProps) {
+export function OauthLoginButton({ host }: OauthLoginButtonProps) {
   const [isLoading, setIsLoading] = useState(false);
+  const { style, handleLogin, imageUrl, labelText } = OauthHosts[host];
 
   const handleClick = () => {
     setIsLoading(true);
@@ -18,22 +44,22 @@ export function OauthLoginButton({ children, className, handleLogin }: OauthLogi
 
   return (
     <div className="space-y-3">
-      <button onClick={handleClick} disabled={isLoading} className="mx-auto block h-12 w-64">
+      <button
+        onClick={handleClick}
+        disabled={isLoading}
+        className={cn('mx-auto block h-12 w-64 rounded-xl px-4 py-2', style)}
+      >
         {isLoading ? (
-          <span
-            className={cn(
-              'flex h-full items-center justify-center gap-2 rounded-xl px-4 py-2',
-              className
-            )}
-          >
+          <span className="flex h-full items-center justify-center gap-2">
             <span className="h-4 w-4 animate-spin rounded-full border-2 border-current border-t-transparent" />
             <span className="font-bold">연결 중...</span>
           </span>
         ) : (
-          <span
-            className={cn('flex items-center justify-center gap-2 rounded-xl px-4 py-2', className)}
-          >
-            {children}
+          <span className="flex h-full items-center justify-center gap-2">
+            <div className="relative size-8">
+              <Image src={imageUrl} className="absolute" fill alt={`${host} 로그인 버튼`} />
+            </div>
+            <span className="font-bold">{labelText}</span>
           </span>
         )}
       </button>
