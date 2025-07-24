10bcf9616e031bcb202db9bbd9c1ee74f1c13d86
위영진 <qser155@naver.com>
2025-07-24 11:38:14 +0900

    refactor: delete redirect page

--- components/features/auth/set-cookie-component.tsx
+++ components/features/auth/set-cookie-component.tsx
@@ -1,8 +1,21 @@
 'use client';
 
+import { SESSION_REDIRECT_KEY } from '@/lib/auth/constants';
 import { UserInfoFromHostServer } from '@/types/auth';
 import { redirect } from 'next/navigation';
 import { useEffect } from 'react';
+
+const redirectPreviousPage = () => {
+  if (window) {
+    const redirectUrl = window.sessionStorage.getItem(SESSION_REDIRECT_KEY);
+    window.sessionStorage.removeItem(SESSION_REDIRECT_KEY);
+    if (!redirectUrl) {
+      redirect('/');
+    } else {
+      redirect(redirectUrl);
+    }
+  }
+};
 
 function SetCookieComponent({ userInfoFromHost }: { userInfoFromHost: UserInfoFromHostServer }) {
   useEffect(() => {
@@ -18,7 +31,8 @@ function SetCookieComponent({ userInfoFromHost }: { userInfoFromHost: UserInfoFr
       if (res.status !== 200) {
         throw new Error('error 발생');
       }
-      redirect('/auth/redirect');
+
+      redirectPreviousPage();
     })();
   }, [userInfoFromHost]);
   return <div></div>;
