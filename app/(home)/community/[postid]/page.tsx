8240161c7146949a6f14dc4ba0180eb85c121837
위영진 <qser155@naver.com>
2025-08-05 18:36:09 +0900

    feat: 커뮤니티 포스트 상세 페이지 UI 제작

--- app/(home)/community/[postid]/page.tsx
+++ app/(home)/community/[postid]/page.tsx
@@ -1,10 +1,15 @@
+import CommunityDetail from '@/components/features/community/community-detail';
+import { getPostById } from '@/lib/community/server';
+
 interface PageProps {
   params: Promise<{ postId: string }>;
 }
 
 async function PostDetailPage({ params }: PageProps) {
   const { postId } = await params;
-  return <div>{postId}</div>;
+  // const post = await getPostById(postId);
+
+  return <CommunityDetail />;
 }
 
 export default PostDetailPage;
