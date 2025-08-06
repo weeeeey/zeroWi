970dfd7fb65ebe933ae15203600c689680616088
위영진 <qser155@naver.com>
2025-08-06 16:00:02 +0900

    feat: 댓글 등록 기능 추가

--- components/features/community/community-detail.tsx
+++ components/features/community/community-detail.tsx
@@ -3,12 +3,18 @@
 import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
+import { toast } from '@/components/ui/custom-toaster';
 import { Separator } from '@/components/ui/separator';
 import { Textarea } from '@/components/ui/textarea';
 import { dummyComments } from '@/dummy';
+import { useUser } from '@/hooks/use-user';
 import { CommunityPostWithAuthorAndComments } from '@/types/community';
+import { Comment } from '@prisma/client';
 import { format } from 'date-fns';
 import { Heart, MessageCircle, Share2 } from 'lucide-react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, useState } from 'react';
 
 import CommunityCreateLinkButton from './community-create-link-button';
 
@@ -29,8 +35,8 @@ export default function CommunityDetail({ post }: { post: CommunityPostWithAutho
 
             <div className="flex items-center gap-4">
               {/* 아바타 */}
-              <div className="size-8 overflow-hidden rounded-full bg-black">
-                <div className="size-full" />
+              <div className="relative size-10 overflow-hidden rounded-full">
+                <Image fill alt="작성자 이미지" src={post.author.picture} />
               </div>
               <div className="flex-1">
                 <div className="mb-1 flex items-center gap-2">
@@ -81,44 +87,110 @@ export default function CommunityDetail({ post }: { post: CommunityPostWithAutho
       </Card>
 
       {/* Comments Section */}
-      <Card className="rounded-none border-none p-0 pb-36">
-        <CardContent className="space-y-4 py-2">
-          {/* Comment Form */}
-          <div>
-            <Textarea placeholder="댓글을 작성해주세요..." className="mb-3" />
-            <div className="flex justify-end">
-              <Button className="bg-indigo-600 hover:bg-indigo-700">댓글 등록</Button>
-            </div>
-          </div>
-
-          <Separator />
-
-          {/* Comments List */}
-          <div className="space-y-6">
-            {dummyComments.map((comment) => (
-              <div key={comment.id} className="flex gap-3">
-                {/* 아바타 */}
-                <div className="size-8 overflow-hidden rounded-full bg-black">
-                  <div className="size-full" />
-                </div>
-                <div className="flex-1">
-                  <div className="mb-1 flex items-center gap-2">
-                    <span className="text-sm font-medium">{comment.author.name}</span>
-                    <span className="text-xs text-gray-500">{comment.createdAt}</span>
-                  </div>
-                  <p className="mb-2 text-sm text-gray-700">{comment.content}</p>
-                  {/* <Button variant="ghost" size="sm" className="h-6 px-2 text-gray-500">
-                    <Heart className="mr-1 h-3 w-3" />
-                    {comment.likes}
-                  </Button> */}
-                </div>
-              </div>
-            ))}
-          </div>
-        </CardContent>
-      </Card>
+      <CommentS comments={post.comments} postId={post.id} />
 
       <CommunityCreateLinkButton />
     </div>
+  );
+}
+
+function CommentS({ comments, postId }: { comments: Comment[]; postId: string }) {
+  const router = useRouter();
+  const { userId } = useUser();
+  const [content, setContent] = useState('');
+  const [isDisabled, setIsDisabled] = useState(false);
+  const handleSubmit = async () => {
+    try {
+      if (!content) {
+        window.alert('댓글 내용을 입력해주세요.');
+        return;
+      }
+      setIsDisabled(true);
+      const res = await fetch('/api/community/post/comment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          content,
+          postId,
+          authorId: userId,
+        }),
+      });
+
+      const parsedData = await res.json();
+      if (!parsedData.success) {
+        throw new Error('댓글 등록 실패');
+      }
+      setContent('');
+      router.refresh();
+    } catch {
+      toast('댓글 등록 실패', {
+        variant: 'danger',
+      });
+    } finally {
+      setIsDisabled(false);
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { value } = e.target;
+    if (value.length >= 199) {
+      window.alert('댓글 내용은 최대 200자 입니다.');
+      return;
+    }
+    setContent(value);
+  };
+
+  return (
+    <Card className="rounded-none border-none p-0 pb-36">
+      <CardContent className="space-y-4 py-2">
+        {/* Comment Form */}
+        <div>
+          <Textarea
+            value={content}
+            onChange={handleChange}
+            placeholder="댓글을 작성해주세요..."
+            disabled={isDisabled}
+            className="mb-3 h-20"
+            maxLength={200}
+          />
+          <div className="flex justify-end">
+            <Button
+              disabled={isDisabled}
+              onClick={handleSubmit}
+              className="bg-indigo-600 hover:bg-indigo-700"
+            >
+              댓글 등록
+            </Button>
+          </div>
+        </div>
+
+        <Separator />
+
+        {/* Comments List */}
+        <div className="space-y-6">
+          {dummyComments.map((comment) => (
+            <div key={comment.id} className="flex gap-3">
+              {/* 아바타 */}
+              <div className="size-8 overflow-hidden rounded-full bg-black">
+                <div className="size-full" />
+              </div>
+              <div className="flex-1">
+                <div className="mb-1 flex items-center gap-2">
+                  <span className="text-sm font-medium">{comment.author.name}</span>
+                  <span className="text-xs text-gray-500">{comment.createdAt}</span>
+                </div>
+                <p className="mb-2 text-sm text-gray-700">{comment.content}</p>
+                {/* <Button variant="ghost" size="sm" className="h-6 px-2 text-gray-500">
+                    <Heart className="mr-1 h-3 w-3" />
+                    {comment.likes}
+                  </Button> */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
   );
 }
